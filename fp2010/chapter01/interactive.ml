c855b20eea14e717691dd024c46f8f98c85d90bb
Pradeep Gowda <pradeep@btbytes.com>
2014-12-16 21:32:04 -0500

    solutions to 2.1

--- fp2010/chapter01/interactive.ml
+++ fp2010/chapter01/interactive.ml
@@ -1,6 +1,33 @@
 (* result of sin(1.21)*2.1^8 *)
 
-(*
+(* 1.
 utop[22]> sin 1.21 *. 2.1 ** 8.0 ;;
 - : float = 353.876724427
+*)
+
+(* 2.
+we cannot write 23 + 11.21 because + is only defined
+for the integer type and not the floating point type
+*)
+
+(* 3.
+concatenate two strings.
+
+   > "Hello" ^ " World";;
+- : string = "Hello World"
+*)
+
+(* 4.
+Evaluate 4 > 2 ^ true
+
+   > 4 > 2 || true ;;
+- : bool = true
+
+*)
+
+(* 5.
+   > print_endline "hello!";;
+hello!
+- : unit = ()
+type of the expressio is `unit`.
 *)
